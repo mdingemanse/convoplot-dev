68ec43c5553051ad2654d62049e102a7d8f75a15
Marlou Rasenberg <marlou.rasenberg@mpi.nl>
2022-10-04 15:21:55 +0200

    layout turns over multiple rows (classictranscript = T)

--- convoplot-dev.R
+++ convoplot-dev.R
@@ -41,21 +41,30 @@ finduid <- function(string) {
 # alldata=F   if TRUE, output all data, not just the selected dyads
 # debug=F     if TRUE, print the selected data and some other diagnostics
 convplot <- function(uids=NULL,lang=NULL,n=10,
+                     classictranscript=F,
                      window=NULL,before=10000,after=10000,
                      printuids=T,verbose=T,
                      dyads=F,content=F,highlight=F,center=F,
                      datamode=F,alldata=F,debug=F) {
+  #check input
+  if(classictranscript & length(uids)>1)
+    stop("for printing classic transcripts, please provide a single uid, or no uids (it will randomly select one") #warning message
+  
   
   if(!is.null(window)) {
     before <- window / 2
     after <- window / 2
   }
   
+  if(classictranscript){
+    n <- 1 #if classic transcript but no uid given, sample 1 uid
+  }
+  
   if(!is.null(uids)) {
     n <- length(uids)
   } else {
     if(verbose) {
-      print(paste('No uids given, sampling',n,'random ones'))
+      print(paste('No uids given, sampling',n,'random one(s)'))
     }
     if(!is.null(lang)) {
       if(verbose) {
@@ -107,6 +116,9 @@ convplot <- function(uids=NULL,lang=NULL,n=10,
   extracts.dyadic <- extracts %>% filter(participation == "dyadic")
   ndyads <- length(unique(extracts.dyadic$scope))
   
+
+    
+  
   if(verbose) {
     print(paste('seeing',ndyads,'dyads in ',n,'non-overlapping extracts'))
   }
@@ -123,6 +135,49 @@ convplot <- function(uids=NULL,lang=NULL,n=10,
     if(alldata) { return(extracts) }
     
     return(extracts.dyadic)
+  }
+  
+  if (classictranscript&highlight){
+    p <- extracts %>%
+      mutate(line = 1:nrow(extracts), 
+             axis_label=paste0(1:nrow(extracts), "   ", LETTERS[1:max(extracts$participant_int)])) %>%
+      ggplot(aes(y=participant_int, fill=focus))+
+      scale_y_reverse(breaks=c(1:max(extracts$participant_int)),
+                      labels=LETTERS[1:max(extracts$participant_int)])+
+      theme_tufte() + theme(legend.position = "none",
+                            strip.placement = "outside")+
+      #theme(axis.title.y.right = element_text(hjust=-10))+
+      ylab("") + xlab("") +
+      theme(axis.ticks.y = element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())+
+      geom_text(aes(label=utterance, color=focus), x=0, nudge_x=-0.5, hjust=0,size=3)+
+      labs(caption=paste0("uid: ", extracts$uid, "\nsource: ",extracts$source, "\nwindow: ", extracts$begin, " ms - ", extracts$end, " ms"))+
+      theme(plot.caption = element_text(color="darkgrey"))
+
+    p <- p + ggforce::facet_col(facets=vars(line), scales="free_y", space="free", strip.position="left")+
+      theme(strip.text.y.left = element_text(angle=0))
+
+    return(p)}
+    
+  if (classictranscript&!highlight){
+      p <- extracts %>%
+        mutate(line = 1:nrow(extracts), 
+               axis_label=paste0(1:nrow(extracts), "   ", LETTERS[1:max(extracts$participant_int)])) %>%
+        ggplot(aes(y=participant_int))+
+        scale_y_reverse(breaks=c(1:max(extracts$participant_int)),
+                        labels=LETTERS[1:max(extracts$participant_int)])+
+        theme_tufte() + theme(legend.position = "none",
+                              strip.placement = "outside")+
+        #theme(axis.title.y.right = element_text(hjust=-10))+
+        ylab("") + xlab("") +
+        theme(axis.ticks.y = element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())+
+        geom_text(aes(label=utterance), x=0, color="black", nudge_x=-0.5, hjust=0,size=3)+
+        labs(caption=paste0("uid: ", extracts$uid, "\nsource: ",extracts$source, "\nwindow: ", extracts$begin, " ms - ", extracts$end, " ms"))+
+        theme(plot.caption = element_text(color="darkgrey"))
+      
+      p <- p + ggforce::facet_col(facets=vars(line), scales="free_y", space="free", strip.position="left")+
+        theme(strip.text.y.left = element_text(angle=0))
+      
+      return(p)
     
   } else {
     
@@ -133,9 +188,13 @@ convplot <- function(uids=NULL,lang=NULL,n=10,
     p <- extracts %>%
       mutate(striplength = case_when(duration < 300 ~ 3,
                                      duration >= 300 ~ round(duration/90)),
-             uttshort = ifelse(nchar <= striplength | nchar <= 4, 
+            uttshort = ifelse(nchar <= striplength | nchar <= 4, 
                                utterance,
                                paste0(stringx::strtrim(utterance,striplength),'~')))%>%
+            # uttshort = ifelse(length(unlist(str_split(utterance,boundary("character"))))>1 && #if utterance is longer than one character
+            #               strwidth(utterance, font = 3, units = 'inches')>duration/893, #AND if utterance is longer than desired width
+            #             lapply(utterance, truncate_string(string=utterance, duration=duration)), #then truncate it (function returns a string)
+            #             utterance))%>%  #or keep as is
       ggplot(aes(y=participant_int)) +
       theme_tufte() + theme(legend.position = "none",
                             strip.placement = "outside",
@@ -147,15 +206,18 @@ convplot <- function(uids=NULL,lang=NULL,n=10,
       geom_rect(aes(xmin=begin0,xmax=end0,ymin=participant_int-0.4,ymax=participant_int+0.4),
                 size=1,fill="grey90",color="white")
     
-    if(highlight) { 
+      if(highlight) { 
       p <- p + geom_rect(data=extracts %>% filter(focus == "focus"),
                          aes(xmin=begin0,xmax=end0,
                              ymin=participant_int-0.4,ymax=participant_int+0.4),
-                         size=1,fill="red",color="white") 
-    }
+                         size=1,fill="red",color="white")}
+    
     if(content) { 
       p <- p + geom_text(aes(label=uttshort,x=begin0+60),
-                         color="black",hjust=0,size=3)
+                          color="black",hjust=0,size=3)
+      # p <- p + ggrepel::geom_text_repel(aes(label=uttshort,x=begin0+60),
+      #                      color="black",hjust=0,size=3)
+      #p <- p + ggfittext::geom_fit_text(aes(xmin=begin0, xmax=end0, label=uttshort), min.size=10, grow=F, padding.x=grid::unit(2, "mm"), padding.y=grid::unit(2, "mm"))
     }
     
     #p <- p + facet_wrap(~ scope, ncol=1, scales="free_y")
@@ -166,7 +228,7 @@ convplot <- function(uids=NULL,lang=NULL,n=10,
     
     return(p)
     
-  }
+    }
 }
 
 # from: https://stackoverflow.com/questions/33629491/dplyr-sample-size-greater-than-population-size
@@ -183,4 +245,7 @@ sample_vals <- function (tbl, size, replace = FALSE, weight = NULL, .env = paren
   idx <- unlist(sampled) + 1
   grouped_df(tbl[idx, , drop = FALSE], vars = groups(tbl))
 }
+
+
+
 
