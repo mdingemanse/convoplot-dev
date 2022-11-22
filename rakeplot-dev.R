eda44da8aa89ab4c362e41e7f9f76b9ba14b966a
mdingemanse <mark@vormdicht.nl>
2022-11-22 22:12:57 +0100

    improved streak detection code

--- rakeplot-dev.R
+++ rakeplot-dev.R
@@ -17,26 +17,43 @@ source("..\\elpaco-lingdiv\\elpaco-lingdiv-0-data_load.R")
 
 # add topturns: highly frequent recurring turn formats
 # add streaks: successive occurrences of top turns by same participant
-d <- d %>% 
+# we set a flag whenever a prior or next turn by same participant is also a top turn,
+# then set streak=1 for consecutive flags and sum the flags to form streak_pos
+d <- d %>%
+  ungroup() %>%
   mutate(topturn = ifelse(rank < 21 & n > 20,1,0)) %>%
-  group_by(language,source) %>%
-  arrange(source,participant) %>% 
-  mutate(flag = ifelse(topturn==1 & (lead(topturn==1) | lag(topturn==1) ),T,F)) %>%
-  mutate(flag = ifelse(is.na(flag),F,flag)) %>%
-  group_by(participant,group=cumsum(flag != lag(flag,default=first(flag)))) %>%
+  group_by(source,participant) %>% 
+  mutate(flag = ifelse(topturn==1 & ( lead(topturn)==1 | lag(topturn)==1),T,F)) %>%
+  mutate(flag = ifelse(is.na(flag),F,flag)) %>% # set all NA flags to F
+  arrange(source,participant) %>% # arrange by source and participant
+  group_by(group=cumsum(flag != lag(flag,default=first(flag)))) %>%
   mutate(streak_pos = if(first(flag)) cumsum(flag) else NA,
          streak = ifelse(!is.na(streak_pos),1,0)) %>%
   ungroup() %>%
   select(-c(flag,group)) %>%
-  arrange(language,uid) 
+  arrange(language,uid)
 
 
 # add measures for chunkiness
 # talk_skew: how skewed is talk in the 10s window preceding and including this turn
 
 d <- d %>%
-  mutate(talk_skew = abs(talk_rel - 0.5),
-         chunk = ifelse((talk_skew > 0.3 & participants==2) | streak==1,1,0))
+  mutate(talk_skew = abs(talk_rel - 0.5))
+
+# chunk type 1: flanked by streak items
+d <- d %>%
+  mutate(chunk = ifelse( lag(streak==1) & lead(streak==1) | streak==1 ,1,0))
+
+# chunk type 2: either highly skewed & dyadic, or flanked by streak items
+
+d <- d %>%
+  mutate(chunk = ifelse((talk_skew > 0.4 & participants==2) 
+                        | lag(streak==1) & lead(streak==1)
+                        | streak==1 ,1,0))
+
+
+
+
 # first let's get metadata at source level
 
 totals_by_source <- d %>% group_by(language,source) %>%
@@ -66,7 +83,7 @@ long_dyadic_convos$firstuid[1]
 
 
 # create dataframe
-extract <- convplot(long_dyadic_convos$firstuid[71],datamode=T,before=0,after=extract_length)
+extract <- convplot(long_dyadic_convos$firstuid[5],datamode=T,before=0,after=extract_length)
 
 # we make lines by categorizing begin values into intervals the width of window_size
 # we drop turns that fall outside the larger interval
@@ -81,7 +98,7 @@ extract <- extract %>%
 
 # and we plot
 extract %>% 
-  ggplot(aes(y=participant_int,fill=talk_skew)) +
+  ggplot(aes(y=participant_int,fill=chunk)) +
   theme_tufte() + theme(legend.position = "none",
                         strip.text = element_blank(),
                         axis.ticks.y = element_blank(),
@@ -92,7 +109,7 @@ extract %>%
   scale_y_reverse(breaks=c(1:2),
                   labels=LETTERS[1:2]) +
   geom_rect(aes(xmin=begin0,xmax=end0,ymin=participant_int-0.6,ymax=participant_int+0.6),
-            size=1) +
+            size=0.3,colour="white") +
   facet_wrap(~line,ncol=1)
 
 # # can of course add a separate layer at some specified threshold
@@ -109,7 +126,7 @@ extract %>%
   scale_y_reverse(breaks=c(1:2),
                   labels=LETTERS[1:2]) +
   geom_rect(aes(xmin=begin0,xmax=end0,ymin=participant_int-0.6,ymax=participant_int+0.6),
-            size=1,colour="white") +
+            size=0.2,colour="white") +
   geom_point(data=extract %>% filter(topturn == 1),
              aes(x=begin0+200),colour="white",size=4,shape=21,stroke=1) +
   geom_point(data=extract %>% filter(streak == 1),
@@ -129,12 +146,12 @@ extract %>%
   ggtitle(paste0(extract$langfull[1]," (",extract$source[1],")"),
           subtitle = "Mapping load to turn height to show competition") +
   ylab("") + xlab("time (ms)") +
-  scale_fill_viridis(option="plasma",direction=1,begin=0.2,end=0.8) +
+  scale_fill_viridis(option="plasma",direction=1,begin=0.1,end=0.9) +
   scale_y_reverse(breaks=c(1:2),
                   labels=LETTERS[1:2]) +
   geom_rect(aes(xmin=begin0,xmax=end0,
-                ymin=participant_int-0.5*load,
-                ymax=participant_int+0.5*load),
+                ymin=participant_int-0.8*load,
+                ymax=participant_int+0.8*load),
             colour="white") +
   # geom_point(data=extract %>% filter(topturn == 1),
   #            aes(x=begin0+200),colour="white",size=4,shape=21,stroke=1) +
